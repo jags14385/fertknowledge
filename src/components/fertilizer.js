c77c36e9d70b5a583543e10698a6b45ab0798481
Jags <jags14385@users.noreply.github.com>
2022-04-01 11:53:37 +0530

    adding fertilizer infop graphql

--- src/components/fertilizer.js
+++ src/components/fertilizer.js
@@ -1,3 +1,5 @@
+import { gql, useQuery } from "@apollo/client";
+
 const FERTLIZER_INFO_QUERY = gql`
   {
     fertilizer_info(where: { product_no: { _eq: 91556 } }) {
@@ -18,4 +20,18 @@ const FERTLIZER_INFO_QUERY = gql`
   }
 `;
 
-export default Fertlizer;
+export default function FertilizerInfo() {
+  const { loading, error, data } = useQuery(FERTLIZER_INFO_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.fertilizer_info.map(
+    ({ product, product_no, product_constitutents }, index) => (
+      <>
+        <div key={index}>{product}</div>
+        <div key={index}>{product_no}</div>
+      </>
+    )
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import React from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/client";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import ExchangeRates from "./components/exchange_rates";
+import { setContext } from "@apollo/client/link/context";
+import FertilizerInfo from "./components/fertilizer";
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      "content-type": "application/json",
+      "x-hasura-admin-secret":
+        "4RnrajgKXZKXhtB4oy4il6jCZAXzk4z6Y5AbaRhwzxrHkKhJn4DOXiQP5C9nynoz",
+    },
+  };
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "https://48p1r2roz4.sse.codesandbox.io",
-  }),
+  link: authLink.concat(
+    new HttpLink({
+      uri: "https://fertilizer-info.hasura.app/v1/graphql",
+    })
+  ),
 });
 
 function App() {
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
-      <ExchangeRates />
+      <FertilizerInfo />
     </div>
   );
 }

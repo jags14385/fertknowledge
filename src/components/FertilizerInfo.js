a18e0b58fbf0e1dc4328b082158aae497c027e5b
Jags <jags14385@users.noreply.github.com>
2022-05-06 11:49:29 +0530

    adding gql query for more info (#5)
    * adding gql query for more info
    
    * Cleanup + Logo refresh
    
    * Tagline decription updated
    
    * Updating description
    
    * Using useEffect for debounce and rendering arrays in React

--- src/components/FertilizerInfo.js
+++ src/components/FertilizerInfo.js
@@ -2,16 +2,35 @@ import { Badge } from "./shared/Badge";
 import { List, ListItem } from "./shared/List";
 
 export default function FertilizerInfo({ newFertilizers }) {
-  console.log("newFertilizers", newFertilizers);
+  const renderDetail = (product_constitutents_details) => {
+    return product_constitutents_details.map((item, index) => {
+      return (
+        <div key={`${item.constitutents_detail.chemical_group}_${index}`}>
+          <div>{item.constitutents_detail.chemical_group}</div>
+          <div>{item.constitutents_detail.constituent_name}</div>
+          <div>{item.constitutents_detail.constituent_code}</div>
+        </div>
+      );
+    });
+  };
   const renderFertlizers = (fertilizers) => {
     if (!fertilizers) {
       return <span>No fertilizers</span>;
     }
-    return fertilizers.map(({ product_no, product }) => (
-      <ListItem>
-        {product_no} || <Badge>{product}</Badge>
-      </ListItem>
-    ));
+    return fertilizers.map(
+      ({ product_no, product, product_constitutents }, index) => {
+        const product_constitutents_details = product_constitutents.map(
+          (item) => item
+        );
+
+        return (
+          <ListItem key={index}>
+            {product_no} || <Badge>{product}</Badge> ||{" "}
+            {renderDetail(product_constitutents_details)}
+          </ListItem>
+        );
+      }
+    );
   };
   return <List>{renderFertlizers(newFertilizers)}</List>;
 }
--- src/components/FertilizerSearch.js
+++ src/components/FertilizerSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 import FertilizerInfo from "./FertilizerInfo";
 import Search from "./Search";
@@ -8,24 +8,38 @@ const SEARCH_FERTILIZER = gql`
     fertilizer_info(where: { product: { _ilike: $match } }) {
       product_no
       product
+      product_constitutents {
+        constitutents_detail {
+          constituent_code
+          constituent_name
+          chemical_group
+        }
+      }
     }
   }
 `;
 
 const FertilizerSearch = () => {
-  const [inputVal, setinputVal] = useState("");
+  const [inputVal, setinputVal] = useState(undefined);
   const [search, { _loading, error, data }] = useLazyQuery(SEARCH_FERTILIZER);
+
+  useEffect(() => {
+    if (inputVal === undefined || inputVal.length > 2) {
+      const stringValue = `%${inputVal}%`;
+      search({ variables: { match: inputVal ? stringValue : `%%` } });
+    }
+  }, [inputVal]);
+
   return (
     <div>
       <Search
         inputVal={inputVal}
         onChange={(e) => {
           setinputVal(e.target.value);
-          search({ variables: { match: `%${e.target.value}%` } });
           // console.log("Input val :::", inputVal);
           // console.log("E target value :::", e.target.value);
         }}
-        //        onSearch={() => search({ variables: { match: `%${inputVal}%` } })}
+        // onSearch={() => search({ variables: { match: `%${inputVal}%` } })}
       />
       {error ? (
         <span>Something went wrong</span>
